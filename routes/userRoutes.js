637038a981df9837bcbb2462e98b8eb1177c3a98
mindeby <debora.marisa.ribeiro@gmail.com>
2020-01-15 16:44:40 +0100

    added user authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,9 +4,40 @@
 const express = require('express');
 const morgan = require('morgan');
 const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
 const User = require('../models').User;
 const Course = require('../models').Course;
 
+const authenticateUser = async (req, res, next) => {
+  let message = null;
+  const credentials = auth(req);
+  if (credentials) {
+    const user = await User.findOne({
+      where: { emailAddress: credentials.name }
+    });
+    console.log(user);
+    if (user) {
+      const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password)
+      if (authenticated) {
+        console.log(`Authentication successful for: ${user.emailAddress}`);
+        req.currentUser = user;
+      } else {
+        message = `Could not authenticate ${user.emailAddress}`;
+      }
+    } else {
+      message = `Could not find the username: ${user.emailAddress}`;
+    }
+  } else {
+    message = `Authenticate header not found`;
+  }
+  if (message) {
+    console.warn(message);
+    res.status(401).json({message: "Access denied" });
+  } else {
+    next();
+  }
+}
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb){
@@ -21,10 +52,10 @@ function asyncHandler(cb){
 
 const router = express.Router();
 
-router.get('/users', asyncHandler(async (req, res) => {
-  const allUsers = await User.findAll();
-  res.json(allUsers);
-}));
+//router.get('/users', asyncHandler(async (req, res) => {
+//  const allUsers = await User.findAll();
+//  res.json(allUsers);
+//}));
 
 router.post('/users', asyncHandler(async (req, res) => {
   const newUser = await User.create({
@@ -34,6 +65,16 @@ router.post('/users', asyncHandler(async (req, res) => {
     password: bcryptjs.hashSync(req.body.password)
   });
   res.status(201).end();
+}));
+
+//PROTECTED routes
+router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
+  const user = req.currentUser;
+  res.status(200).json({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    username: user.emailAddress,
+  })
 }));
 
 module.exports = router;
